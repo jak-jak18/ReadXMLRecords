1615db1f582ea168f55958eac1a7796d7f759fd7
jak jak <jkatzwhite@gmail.com>
2023-01-30 14:58:59 -0500

    Added default controller.

---
+++ ReadXMLRecords/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadXMLRecords
+{
+    internal class
+    {
+         public class DefaultController : Controller
+    {
+        /// <summary>
+        /// GET: /Default/
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Robots()
+        {
+            Response.ContentType = "text/plain";
+            return View();
+        }
+    }
+}
+}
